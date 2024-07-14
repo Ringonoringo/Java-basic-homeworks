7f8c05b83a6c218dd133b05e14e1a4ce4b4a46a2
{nabuhiro} <{nabuhiro@gmail.com}>
2024-07-14 17:35:14 +0300

    Задание 2 3го урока готово

--- src/main/java/ru/nabuhiro/java/basic/homeworks/lesson3/MainApplication.java
+++ src/main/java/ru/nabuhiro/java/basic/homeworks/lesson3/MainApplication.java
@@ -4,8 +4,20 @@ public class MainApplication {
         public static void main(String[] args) {
             //Каждый метод последовательно вызовите из метода main()
             greetings();
+            checkSign(1,-5,3);
         }
         //(1) Реализуйте метод greetings(), который при вызове должен отпечатать в столбец 4 слова: Hello, World, from, Java;
         public static void greetings() {
             System.out.println("Hello\nWorld\nfrom\nJava");}
+
+    //(2) Реализуйте метод checkSign(..), принимающий в качестве аргументов 3 int переменные a, b и c.
+    // Метод должен посчитать их сумму, и если она больше или равна 0, то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”;
+    public static void checkSign(int a,int b,int c) {
+        int sum = a+b+c;
+        if (sum>=0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
 }
