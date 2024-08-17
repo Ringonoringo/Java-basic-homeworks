c16f8464688acc35de85383cf66dd0c9eb9bfedb
{nabuhiro} <{nabuhiro@gmail.com}>
2024-08-17 22:56:58 +0300

    init

---
+++ src/main/java/ru/nabuhiro/java/basic/homeworks/lesson14/AppArraySizeException.java
@@ -0,0 +1,8 @@
+package ru.nabuhiro.java.basic.homeworks.lesson14;
+
+public class AppArraySizeException extends RuntimeException {
+    public AppArraySizeException(String message) {
+        super(message);
+    }
+
+}
