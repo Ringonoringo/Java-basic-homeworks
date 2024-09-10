063128057df070fa1f62cf171d14f27540a18c1d
Ringonoringo <nabuhiro@gmail.com>
2024-09-11 00:44:34 +0300

    Delete src/main/java/ru/nabuhiro/java/basic/homeworks/lesson17/SearchTree.java

--- src/main/java/ru/nabuhiro/java/basic/homeworks/lesson17/SearchTree.java
+++
@@ -1,8 +0,0 @@
-package ru.nabuhiro.java.basic.homeworks.lesson17;
-import java.util.List;
-public interface SearchTree {
-        Integer find(Integer element);
-        List<Integer> getSortedList();
-
-
-}
