66d8af3db54561c2a3a7a32a7ec70923272ea9b4
{nabuhiro} <{nabuhiro@gmail.com}>
2024-09-11 00:39:59 +0300

    для проверки

---
+++ src/main/java/ru/nabuhiro/java/basic/homeworks/lesson17/pp/Position.java
@@ -0,0 +1,6 @@
+package ru.nabuhiro.java.basic.homeworks.lesson17.pp;
+
+public enum Position {
+    MANAGER, DIRECTOR, DRIVER, ENGINEER, SENIOR_MANAGER, DEVELOPER, QA,
+    JANITOR, PLUMBER, BRANCH_DIRECTOR, JUNIOR_DEVELOPER
+}
