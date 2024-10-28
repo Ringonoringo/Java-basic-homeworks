25c4b8988b362d699937882d7bcfa01b58216096
Ringonoringo <nabuhiro@gmail.com>
2024-10-28 21:15:58 +0300

    корректировки добавила в таблицу ответов id вопроса по другому составила ссылки убрала лишнее от интерфейса посгресс

--- src/main/java/ru/nabuhiro/java/basic/homeworks/lesson24/questiontest.sql
+++ src/main/java/ru/nabuhiro/java/basic/homeworks/lesson24/questiontest.sql
@@ -1,45 +1,18 @@
--- Table: public.qwestions
-
--- DROP TABLE IF EXISTS public.questions;
-
-CREATE TABLE IF NOT EXISTS public.questions
+CREATE TABLE public.questions
 (
-    question_id integer NOT NULL DEFAULT nextval('questions_question_id_seq'::regclass),
-    question_text text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT questions_pkey PRIMARY KEY (question_id)
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
+    question_id serial NOT NULL DEFAULT,
+    question_text text NOT NULL,
 
-ALTER TABLE IF EXISTS public.questions
-    OWNER to postgres;
-
-    -- Table: public.answer
-
-    -- DROP TABLE IF EXISTS public.answer;
-
-    CREATE TABLE IF NOT EXISTS public.answer
+);
+    CREATE TABLE public.answer
     (
-        answer_text text COLLATE pg_catalog."default" NOT NULL,
+        answer_id serial NOT NULL,
+        answer_text text NOT NULL,
         is_true boolean NOT NULL,
-        answer_id integer NOT NULL DEFAULT nextval('answer_answer_id_seq'::regclass),
-        CONSTRAINT answer FOREIGN KEY (answer_id)
+        question_id integer NOT NULL,
+        PRIMARY KEY (answer_id),
+        FOREIGN KEY (question_id)
             REFERENCES public.questions (question_id) MATCH SIMPLE
-            ON UPDATE NO ACTION
-            ON DELETE NO ACTION
-            NOT VALID
-    )
-    WITH (
-        OIDS = FALSE
-    )
-    TABLESPACE pg_default;
-
-    ALTER TABLE IF EXISTS public.answer
-        OWNER to postgres;
------------
-TEST case
-SELECT * from questions, answer
-	where is_true = true and answer_id = question_id
-order by questions.question_id
+            ON UPDATE RESTRICT
+            ON DELETE RESTRICT
+    );
